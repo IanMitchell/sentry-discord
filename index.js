f3ff327db8e74b1a6eca286ecb3cd916083ee602
Ian Mitchell <ianm@zillowgroup.com>
2018-12-14 16:55:26 -0800

    Update for Zeit Lambda

--- index.js
+++ index.js
@@ -4,39 +4,35 @@ const handler = require('serve-handler');
 const fetch = require('node-fetch');
 
 const COLORS = {
-  'debug': parseInt('fbe14f', 16),
-  'info': parseInt('2788ce', 16),
-  'warning': parseInt('f18500', 16),
-  'error': parseInt('e03e2f', 16),
-  'fatal': parseInt('d20f2a', 16),
+  debug: parseInt('fbe14f', 16),
+  info: parseInt('2788ce', 16),
+  warning: parseInt('f18500', 16),
+  error: parseInt('e03e2f', 16),
+  fatal: parseInt('d20f2a', 16),
 };
 
 module.exports = async (request, response) => {
-  if (request.method === 'GET') {
-    return handler(request, response);
-  }
-
   try {
     const body = await json(request);
 
     const payload = {
       username: 'Sentry',
-      avatar_url: `${process.env.NOW_URL}/sentry-icon.png`,
+      avatar_url: `https://raw.githubusercontent.com/IanMitchell/sentry-discord/master/sentry-icon.png`,
       embeds: [
         {
           title: body.project_name,
           type: 'rich',
           description: body.message,
           url: body.url,
-          timestamp: (new Date(body.event.received * 1000)).toISOString(),
+          timestamp: new Date(body.event.received * 1000).toISOString(),
           color: COLORS[body.level] || COLORS.error,
           footer: {
             icon_url: 'https://github.com/fluidicon.png',
             text: 'ianmitchell/sentry-discord',
           },
-          fields: []
-        }
-      ]
+          fields: [],
+        },
+      ],
     };
 
     if (body.event.user) {
@@ -64,4 +60,4 @@ module.exports = async (request, response) => {
   } catch (err) {
     console.error(err);
   }
-}
+};
